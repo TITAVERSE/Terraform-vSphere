49db890f913d114e5cce1032d4410514bd8703b2
STX Tiavina <tiavina.randriamananjo+gt@stellar-ix.com>
2023-04-12 13:59:10 +0300

    add files

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.3.1"
+    }
+  }
+}
+provider "vsphere" {
+  vsphere_server       = var.vsphere_vcenter
+  user                 = var.vsphere_username
+  password             = var.vsphere_password
+  allow_unverified_ssl = true
+}
