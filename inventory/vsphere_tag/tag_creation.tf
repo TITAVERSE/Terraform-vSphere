de1a18755d6085cd76c84fb85ebdc13b140d58fc
STX Tiavina <tiavina.randriamananjo+gt@stellar-ix.com>
2024-05-17 11:50:09 +0300

    correction datasource vsphere_category

--- inventory/vsphere_tag/tag_creation.tf
+++ inventory/vsphere_tag/tag_creation.tf
@@ -1,6 +1,6 @@
 ## Creation of 1 tag
 resource "vsphere_tag" "tag" {
   name = var.tag_name
-  category_id = "${vsphere_tag_category.category.id}"
+  category_id = "${data.vsphere_tag_category.category.id}"
   description = var.tag_description
 }
