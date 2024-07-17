e2442b1191fbc8c2df2a3dfb0b00b959d95a4630
STX Tiavina <tiavina.randriamananjo+gt@stellar-ix.com>
2024-07-17 12:03:48 +0300

    add variables type

--- inventory/vsphere_tag/variables.tf
+++ inventory/vsphere_tag/variables.tf
@@ -2,38 +2,39 @@
 variable "vsphere_vcenter" {
   type        = string
   description = "VMWare vCenter server FQDN / IP"
-  nullable = false
+  nullable    = false
 }
 
 variable "vsphere_username" {
   type        = string
   description = "vSphere username"
   sensitive   = true
-  nullable = false
+  nullable    = false
 }
 
 variable "vsphere_password" {
   description = "vsphere user password"
   type        = string
   sensitive   = true
-  nullable = false
+  nullable    = false
 }
 
 ## vSphere tag
 
 variable "tag_name" {
   description = "Name of the tag cateogry"
-  nullable = false
+  type        = string
+  nullable    = false
 }
 
 variable "tag_description" {
-  type = string
+  type        = string
   description = "Description of the category"
-  nullable = true
+  nullable    = true
 }
 
 # Category name is used by data
 variable "cat_name" {
   description = "Name of the tag cateogry"
-  nullable = false
+  nullable    = false
 }
