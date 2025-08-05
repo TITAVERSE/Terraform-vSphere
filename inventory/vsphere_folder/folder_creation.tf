4114f8745d132cfaf074102a347b287629a27ce4
stxtra <stxtra@stxlux.local>
2025-08-05 20:01:52 +0300

    folder var correction

--- inventory/vsphere_folder/folder_creation.tf
+++ inventory/vsphere_folder/folder_creation.tf
@@ -1,5 +1,5 @@
 resource "vsphere_folder" "folder" {
-  path = var.vsphere_folder_name
+  path = var.vsphere_folder_fullpath
   type = var.vsphere_folder_type
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
