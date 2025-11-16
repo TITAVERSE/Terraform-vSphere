c7dba54b16156100fa951e8e2d75fc15640a4f9a
ttvtra <ttvtra@titalux.ttv>
2025-11-16 22:35:53 +0300

    windows option add

--- virtual_machine/vsphere_virtual_machine/windows_cloning/variables.tf
+++ virtual_machine/vsphere_virtual_machine/windows_cloning/variables.tf
@@ -1,58 +1,123 @@
+### vCenter access ###
+variable "vsphere_vcenter" {
+  type        = string
+  description = "VMWare vCenter server FQDN / IP"
+}
+
+variable "vsphere_username" {
+  type        = string
+  description = "vSphere username"
+  sensitive   = true
+}
+
+variable "vsphere_password" {
+  description = "vsphere user password"
+  type        = string
+  sensitive   = true
+}
+
+### Metadata
+variable "vm_tags" {
+  type = list(object({
+    category_name = string
+    tag_name      = string
+  }))
+  default = [{
+    category_name = "provisioner"
+    tag_name      = "terraform"
+
+    },
+    {
+      category_name = "status"
+      tag_name      = "prod"
+    },
+    {
+      category_name = "customer"
+      tag_name      = "ttv"
+    }
+  ]
+}
+
+variable "custom_attribute" {
+  default     = true
+  description = "custom attributes to pu on VM"
+  nullable    = true
+}
+
 ### Datacenter ####
-variable "vsphere_datacenter" {
+variable "vsphere_dc" {
+  type        = string
   description = "Datacenter name in vSphere"
   default     = ""
 }
 
-variable "vsphere_datastore" {
-  type        = string
-  description = "Datastore Name in vSphere"
-  default     = ""
-}
-
-variable "vsphere_network" {
-  type        = string
-  description = "Network name in vCenter"
-  default     = ""
-}
-
-variable "vsphere_cluster_host" {
+variable "vsphere_cls_host" {
   type        = string
   description = "Cluster name in a Datacenter"
   default     = ""
 }
 
-variable "vsphere_cluster_datastore" {
+variable "vsphere_resource_pool" {
   type        = string
-  description = "Datastore cluster name"
-  default     = "STXLABCLDVMFS001"
+  description = "Ressource pool for VM"
+  default     = ""
+  nullable    = true
+}
+
+variable "vsphere_host" {
+  type        = string
+  description = "ESXi host"
+  default     = ""
+}
+
+variable "vsphere_ds" {
+  type        = string
+  description = "Datastore name"
+  default     = ""
+}
+
+variable "vsphere_folder" {
+  type    = string
+  default = "Discovered virtual machines"
 }
 
 ### VM Specs ###
 
+variable "vm_name" {
+  type        = string
+  description = "VM Name"
+  default     = "STXVM"
+}
+
 variable "vm_hostname" {
   type        = string
-  description = "Name of the VM"
+  description = "VM's hostname"
   default     = "localhost"
+}
+
+variable "vm_firmware" {
+  type        = string
+  description = "efi or bios"
+  default     = "efi"
 }
 
 variable "vm_cpu_socket" {
   type        = number
-  description = "Number of socket cpu"
+  description = "Number of core per cpu"
   default     = 1
   validation {
-    condition     = var.vm_cpu_socket >= 1 && var.vm_cpu_socket <= 4
-    error_message = "The value of vmcount must be between 1 and 5."
+    condition     = var.vm_cpu_socket >= 1
+    error_message = "The value of vmcount must be superior to 1"
   }
 }
 
 variable "vm_cpu_core" {
   type        = number
-  description = "Number of core per cpu"
-  default     = 1
+  description = "Number of socket"
+  default     = 2
   validation {
-    condition     = var.vm_cpu_core >= 1 && var.vm_cpu_core <= 4
-    error_message = "The value of vm_cpu_core must be between 1 and 4."
+    condition     = var.vm_cpu_core >= 1
+    error_message = "The value of vmcount must be superior to 1"
   }
 }
 
@@ -61,106 +126,128 @@ variable "vm_ram" {
   description = "Quantity of RAM in GB"
   default     = 4
   validation {
-    condition     = var.vm_ram >= 2 && var.vm_ram <= 128
-    error_message = "The value of vm_ram must be between 2 and 128."
+    condition     = var.vm_ram > 0
+    error_message = "The value of vm_ram must be between 0"
   }
 }
 
-variable "vm_dns_servers" {
-  type        = list(string)
-  description = "List of DNS servers"
-  default     = ["8.8.8.8", "8.8.4.4", "1.1.1.1"]
+
+variable "vm_disks" {
+  description = "Configuration of one (1) disk"
+  type = list(object({
+    size        = number
+    unit_number = number
+  }))
+  default = [
+    {
+      size        = 50
+      unit_number = 0
+    },
+    {
+      size        = 100
+      unit_number = 1
+    }
+  ]
 }
 
-variable "vm_ipv4_address" {
+variable "vm_networks" {
+  description = "Network interfaces to attach to the VM"
+  type = list(object(
+    {
+      network         = string
+      vm_ipv4_address = string
+      vm_ipv4_netmask = number
+
+    }
+  ))
+  default = [
+    {
+      network         = "VM Network"
+      vm_ipv4_address = "10.0.0.1"
+      vm_ipv4_netmask = 24
+    }
+  ]
+}
+
+variable "vm_ipv4_ns" {
+  description = "Name servers IP"
   type        = list(string)
-  description = "IPv4 addresses for a vm"
+  default     = ["8.8.8.8", "8.8.4.4"]
+}
+
+variable "vm_dns_suffixes" {
+  description = "DNS suffixes"
+  type        = string
+  default     = ""
+  nullable    = true
 }
 
 variable "vm_ipv4_gateway" {
   type        = string
   description = "IPv4 address of the Gateway"
-}
-
-variable "vm_ipv4_netmask" {
-  type        = string
-  description = "Netmask"
-}
-
-variable "vm_public_key" {
-  type        = string
-  description = "Public key to be used to ssh into a machine"
-  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDR5IBKXMaxdiOt/p0lKX/cl4li5sEfCGoNZ2IrsPojUsM8szWii+4A9ZiOmwYPBgdd6HF5bXFH5nDUMexMpFVjIRR6wpQ1Mvh4lnOmFz3v0bHDmovxpnk1cYHkDGaIo1zWzLwSR3/DLeNDv5pmHUUz5ZF7M3NkpgILWYjus2y8Vuwxwn6uXHpzFuxc2J+KA0HlKKGfgCb026AejEtwf4J/NVOpEndwck1DkBw3jNQtaqVUEReAvCVJnehQzAfnQmbg7VTvWPxftwX+Ml64O04hmbui2Qgv6CePYB91GtzKgDubzAHfWduD3E9ju1/9h9aVFgLJRS9AGlS6j7l2xekZ2qVivkeGwzcNg9neA1DGDssVYszWfOABNV18VLLWIko8HolETAr+/YImTXTVXRaRbjkaGMdiNssOVbBzaNf+42HqA/DmrNO8UV8VWvirb1ai3yeHTU6DYHsnS85rC846KCyQcC1McBQpRcIHCNXqS7rG3TTTofyiMfHNHuJbwj4lPBx2ZhyiWtLxwVQQEWft4GKaCQmPzKR8Qt6Aj5rYwq+pilF3T2du7y5Iz6Q/1y3y0xH/7hA4iKpj82nRBGuwIagfDivzqmx3vzaDBaBOLQoLLvnWunkkuqSxuK+4jAWJrPOQjR6KRbKsUHL7FqeCu0CMW1mj7QBsEO10+E4WIw== stxtra@IX0035-PC"
-}
-
-variable "vm_lin_ssh_username" {
-  type      = string
-  sensitive = true
-  default   = "sam"
+  default     = "10.0.0.253"
 }
 
 variable "vm_template" {
-  description = "Nom du template sur vSphere"
+  description = "VM Template name in vSphere"
   type        = string
 }
 
 variable "template_folder" {
-  description = "dossier contenant les templates de VM"
+  description = "vSphere folder containing Templates"
   type        = string
-}
-
-variable "vm_ipv4_ns" {
-  description = "Liste des serveurs DNS"
-  type        = list(any)
-  default     = ["8.8.8.8", "8.8.4.4"]
 }
 
 variable "customer_domain_name" {
   description = "Local domain AD/LDAP"
   type        = string
-  default     = "stxlab.local"
-}
-
-variable "cust_trigramm" {
-  description = "Trigramme du client"
-  type        = string
-  default     = "STX"
-}
-
-variable "disk_size" {
-  type = number
-  description = "Size in GB of the disk"
-  default = 16
+  default     = ""
 }
 
 variable "guest_OS" {
-  description = "OS to install, Linux, Windows, MacOS"
+  description = "OS to install, Linux, Windows, FreeBSD"
   nullable    = true
   default     = ""
 }
 
 variable "cpu_hot_add_remove" {
-  type = bool
+  type        = bool
   description = "Enable adding/removing CPU while Powered on. Enabling/disabling this setup requires reboot."
-  default = true
+  default     = true
+}
+
+variable "cpu_hot_remove_enabled" {
+  type        = bool
+  description = "Allow CPUs to be removed to the virtual machine while it is powered on."
+  default     = true
 }
 
 variable "ram_hot_add" {
-  type = bool
+  type        = bool
   description = "Enable adding RAM while Powered on. Enabling/disabling this setup requires reboot."
-  default = true
+  default     = true
 }
 
-variable "tag_name" {
-  type = string
-  default = ""
-  description = "tag's name"
-  nullable = true
+variable "vm_time_zone" {
+  type        = string
+  description = "Timezone of the VM"
+  default     = "Africa/Nairobi"
 }
 
-variable "tag_category" {
-  type = string
-  default = ""
-  description = "tag description"
-  nullable = true
+variable "wait_for_guest_net_timeout" {
+  type        = number
+  description = "timeout for networking"
+  default     = 0
+}
+
+variable "efi_secure_boot_enabled" {
+  type        = bool
+  description = "Use this option to enable EFI secure boot"
+  default     = true
+}
+
+variable "enable_logging" {
+  type        = bool
+  description = "Enable logging of virtual machine events to a log file stored in the virtual machine directory"
+  default     = true
 }
---
+++ virtual_machine/vsphere_virtual_machine/windows_cloning/vm_resources_creation.tf
@@ -0,0 +1,74 @@
+## Creating one (1) VM :
+resource "vsphere_virtual_machine" "vm_windows" {
+  resource_pool_id = var.vsphere_resource_pool != "" ? data.vsphere_resource_pool.resource_pool[0].id : data.vsphere_compute_cluster.cls_hosts.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  folder           = var.vsphere_folder
+  tags = [
+    for tag in data.vsphere_tag.tags : tag.id
+  ]
+
+  name                   = var.vm_name
+  firmware               = var.vm_firmware
+  num_cpus               = var.vm_cpu_socket
+  num_cores_per_socket   = var.vm_cpu_core
+  memory                 = var.vm_ram * 1024
+  guest_id               = data.vsphere_virtual_machine.template.guest_id
+  cpu_hot_add_enabled    = var.cpu_hot_add_remove
+  cpu_hot_remove_enabled = var.cpu_hot_add_remove
+  memory_hot_add_enabled = var.ram_hot_add
+
+  efi_secure_boot_enabled = var.efi_secure_boot_enabled
+  enable_logging          = var.enable_logging
+
+  wait_for_guest_net_timeout = var.wait_for_guest_net_timeout
+
+  dynamic "network_interface" {
+    for_each = data.vsphere_network.networks
+    content {
+      network_id   = network_interface.value.id
+      adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+    }
+  }
+
+  dynamic "disk" {
+    for_each = var.vm_disks
+    content {
+      label            = format("%s-disk-%d", var.vm_hostname, disk.value.unit_number)
+      size             = disk.value.size != "" || disk.value.size <= data.vsphere_virtual_machine.template.disks.0.size ? disk.value.size : data.vsphere_virtual_machine.template.disks.0.size
+      unit_number      = disk.value.unit_number
+      thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+      eagerly_scrub    = data.vsphere_virtual_machine.template.disks.0.eagerly_scrub
+    }
+  }
+
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      windows_options {
+        computer_name  = var.vm_hostname
+        workgroup      = var.customer_domain_name
+        time_zone      = var.vm_time_zone
+      }
+
+      dynamic "network_interface" {
+        for_each = var.vm_networks
+        content {
+          ipv4_address = network_interface.value.vm_ipv4_address
+          ipv4_netmask = network_interface.value.vm_ipv4_netmask
+        }
+      }
+
+      ipv4_gateway    = var.vm_ipv4_gateway
+      dns_server_list = var.vm_ipv4_ns
+    }
+  }
+  lifecycle {
+    ignore_changes = [
+      disk,
+      clone,
+      guest_id,     #change of this causes system reboot
+      ept_rvi_mode, #change of this causes system reboot
+      hv_mode,      #change of this causes system reboot
+    ]
+  }
+}
